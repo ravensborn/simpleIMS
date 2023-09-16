bdb5098680dfbdc3d6d1a3ba29687ea53fbe5c2b
Yad <yad.hoshyar1@gmail.com>
2023-09-16 12:36:30 +0300

    Updated order delete from customers page.

--- app/Livewire/Dashboard/Customers/Orders.php
+++ app/Livewire/Dashboard/Customers/Orders.php
@@ -3,6 +3,7 @@
 namespace App\Livewire\Dashboard\Customers;
 
 use App\Models\Customer;
+use App\Models\Order;
 use Illuminate\Pagination\LengthAwarePaginator;
 use Jantinnerezo\LivewireAlert\LivewireAlert;
 use Livewire\Component;
@@ -11,12 +12,52 @@ use Livewire\WithPagination;
 class Orders extends Component
 {
     use WithPagination, LivewireAlert;
+
+    protected $listeners = [
+        'deleteItem',
+    ];
+
     public string $search = '';
+    public int $itemIdToBeDeleted = 0;
+
     public Customer $customer;
     public function mount($customer): void
     {
 
 
+    }
+
+    public function triggerDeleteItem($item): void
+    {
+        $this->confirm('Are you sure that you want to delete this item?', [
+            'toast' => false,
+            'position' => 'center',
+            'showConfirmButton' => true,
+            'cancelButtonText' => 'Cancel',
+            'confirmButtonText' => 'Yes',
+            'onConfirmed' => 'deleteItem'
+        ]);
+
+        $this->itemIdToBeDeleted = $item;
+
+    }
+
+    public function deleteItem(): void
+    {
+
+        $order = Order::find($this->itemIdToBeDeleted);
+
+        if ($order) {
+
+            $order->delete();
+
+            $this->alert('success', 'Item successfully deleted.', [
+                'position' => 'top-end',
+                'timer' => 5000,
+                'toast' => true,
+            ]);
+
+        }
     }
     public function render()
     {
