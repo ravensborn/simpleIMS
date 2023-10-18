e322af3dc0c87a1fa7b8619708da974d56db6471
Yad <yad.hoshyar@gmail.com>
2023-10-19 01:42:28 +0300

    Update database.php

--- config/database.php
+++ config/database.php
@@ -68,7 +68,7 @@ return [
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('LEGACY_DB_DATABASE', 'forge'),
+            'database' => env('LEGACY_DB_DATABASE', 'hiwaold'),
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
