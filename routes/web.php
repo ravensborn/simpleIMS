2318a48c72f3f887fd6e02be971c943613ba4fd0
Yad <yad.hoshyar@gmail.com>
2023-10-19 01:30:25 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -44,8 +44,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/migrate', [MainController::class, 'migrate']);
-
 Auth::routes(['register' => false, 'logout' => false]);
 
 Route::get('/logout', function () {
